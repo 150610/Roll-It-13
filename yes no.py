bb57387e78bae4af1218a90b014c7cfd8f3141e4
150610 <henryp4211@masseyhigh.school.nz>
2026-02-09 09:10:47 +1300

    Add files via upload

--- yes no.py
+++ yes no.py
@@ -1,9 +1,17 @@
-want_instructions = input("Do you want to see the instructions? ").lower()
 
-#check if the user says yes/no
-if want_instructions == "yes": or want_instructions == "y" :
-    print("You said yes")
-elif want_instructions == "no": or want_instructions == "n" :
-    print("You said no")
-else:
-    print("Please answer yes/no")
+while True:
+
+    want_instructions = input("Do you want to see the instructions? ").lower()
+
+    #check if the user says yes/no
+    if want_instructions == "yes": or want_instructions == "y" :
+        print("You said yes")
+        break
+    elif want_instructions == "no": or want_instructions == "n" :
+        print("You said no")
+        break
+    else:
+        print("Please answer yes/no")
+        continue
+
+print("Done")
